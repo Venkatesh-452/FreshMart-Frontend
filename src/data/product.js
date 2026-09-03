d679985a83933c046d1a3debd09895ff41f21b97
Venkatesh-452 <luckyvenkat452@gmail.com>
2026-09-03 14:58:49 +0530

    Create src/data/product.js

---
+++ src/data/product.js
@@ -0,0 +1,10 @@
+const products=[
+{id:1,name:'Tomato',price:40,unit:'kg',category:'Fresh Vegetables',image:'🍅'},
+{id:2,name:'Carrot',price:50,unit:'kg',category:'Root Vegetables',image:'🥕'},
+{id:3,name:'Broccoli',price:80,unit:'kg',category:'Fresh Vegetables',image:'🥦'},
+{id:4,name:'Onion',price:45,unit:'kg',category:'Fresh Vegetables',image:'🧅'},
+{id:5,name:'Potato',price:35,unit:'kg',category:'Root Vegetables',image:'🥔'},
+{id:6,name:'Spinach',price:25,unit:'bunch',category:'Leafy Vegetables',image:'🥬'},
+{id:7,name:'Mushroom',price:55,unit:'kg',category:'Fresh Vegetables',image:'🍄'},
+{id:8,name:'Brinjal',price:25,unit:'kg',category:'Fresh Vegetables',image:'🍆'}]
+export default products
